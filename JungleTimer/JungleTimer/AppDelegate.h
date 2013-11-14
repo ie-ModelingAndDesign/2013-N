fcf10687f6e18858576fc94057830458f6228d0d
innparusu <innparusu95@yahoo.co.jp>
2013-11-14 11:16:18 +0900

    Add JungleTimer Project

---
+++ JungleTimer/JungleTimer/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JungleTimer
+//
+//  Created by Tatsuki IHA on 2013/11/14.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JungleTimer/JungleTimer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JungleTimer
+//
+//  Created by Tatsuki IHA on 2013/11/14.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
