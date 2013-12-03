da83a543ff0195858472a7ce557fee3c2d0c3f1d
innparusu <innparusu95@yahoo.co.jp>
2013-12-04 05:52:50 +0900

    Add Summoner’sRiftClass

---
+++ JungleTimer/JungleTimer/Summoner'sRift.h
@@ -0,0 +1,13 @@
+//
+//  Summoner'sRift.h
+//  JungleTimer
+//
+//  Created by Tatsuki IHA on 2013/12/04.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Summoner_sRift : UIViewController
+
+@end
---
+++ JungleTimer/JungleTimer/Summoner'sRift.m
@@ -0,0 +1,38 @@
+//
+//  Summoner'sRift.m
+//  JungleTimer
+//
+//  Created by Tatsuki IHA on 2013/12/04.
+//  Copyright (c) 2013年 Tatsuki IHA. All rights reserved.
+//
+
+#import "Summoner'sRift.h"
+
+@interface Summoner_sRift ()
+
+@end
+
+@implementation Summoner_sRift
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
